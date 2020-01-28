e7a8550378bd212f008e05aa703155fc7bd16d59
julio quintana <julioeq29@gmail.com>
2020-01-28 11:42:02 +0000

    fighting with sinatra

--- app.rb
+++ app.rb
@@ -7,6 +7,45 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
+require_relative 'recipe'
+require_relative 'cookbook'
+
+
 get '/' do
-  'I\'m here!'
+  csv_file   = File.join(__dir__, 'recipes.csv')
+  cookbook   = Cookbook.new(csv_file)
+  @recipes   = cookbook.all
+  erb :index
 end
+
+get '/new' do
+  erb :new
+end
+
+post '/recipes' do
+  csv_file   = File.join(__dir__, 'recipes.csv')
+  cookbook   = Cookbook.new(csv_file)
+  recipe     = Recipe.new(params[:name],
+                          params[:description],
+                          params[:prep_time],
+                          params[:difficulty])
+  cookbook.add_recipe(recipe)
+  redirect to '/'
+end
+
+get '/recipes' do
+  csv_file   = File.join(__dir__, 'recipes.csv')
+  cookbook   = Cookbook.new(csv_file)
+  cookbook.remove_recipe(params[:index].to_i)
+  redirect to '/'
+end
+
+# get '/about' do
+#   erb :about
+# end
+
+# get '/team/:username' do
+#   # binding.pry
+#   puts params[:username]
+#   "The username is #{params[:username]}"
+# end
---
+++ recipe.rb
@@ -0,0 +1,10 @@
+class Recipe
+  attr_reader :name, :description, :prep_time, :difficulty
+
+  def initialize(name, description, prep_time, difficulty)
+    @name = name
+    @description = description
+    @prep_time = prep_time
+    @difficulty = difficulty
+  end
+end
